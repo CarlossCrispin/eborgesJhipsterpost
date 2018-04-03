03174994f6109e8d9bb67de44624722e2bb07d66
LEVIATAN <carlos.crispin.cc@gmail.com>
2018-04-02 23:04:02 -0500

    modificando home

---
+++ src/main/webapp/app/entities/investigador/tabla.js
@@ -0,0 +1,43 @@
+$('#local').dynatable({
+    table: {
+      defaultColumnIdStyle: 'trimDash'
+    }
+  });
+
+// $(document).ready( function() {
+// 	$('#local').dynatable({
+// 		table: {
+// 		  defaultColumnIdStyle: 'trimDash'
+// 		},
+// 		features: {
+// 		  paginate: true,
+// 		  search: true,
+// 		  recordCount: false,
+// 		  perPageSelect: true
+// 		},
+		
+// 	});    	
+// });
+
+
+// $(document).ready(function() {
+// 	$('#local').DataTable( {
+// 			//dom: 'Bfrtip',
+			
+// 			/*"oTableTools": {
+// 					"sSwfPath": "js/plugins/dataTables/swf/copy_csv_xls_pdf.swf"
+// 				},*/
+// 				"language": {
+// 					"search": "Buscar:",
+// 					"zeroRecords": "No se encontraron datos",
+// 					"infoEmpty": "No hay datos para mostrar",
+// 					"info": "Mostrando del START al END, de un total de TOTAL entradas",
+// 					"paginate": {
+// 							"first": "Primeros",
+// 							"last": "Ultimos",
+// 							"next": "Siguiente",
+// 							"previous": "Anterior"
+// 					},
+// 			},
+// 	} );
+// } );
